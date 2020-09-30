196e63f506f3bd27da3f8133ddcee301a2a4b465
rendytantiono <rendy.tantiono@tokopedia.com>
2020-09-30 23:34:26 +0800

    adding commit generator install

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ setup-semantic:
 	@bash ${PWD}/scripts/setup-semantic.sh
 setup-linter:
 	@bash ${PWD}/scripts/setup-linter.sh
+setup-generator:
+	@bash ${PWD}/scripts/commit-generator.sh
---
+++ scripts/commit-generator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. $(dirname "$0")/utils/chalk.sh
+
+h2Info "Downloading commit generator"
+git clone "https://github.com/lintingzhen/commitizen-go"
+
+h2Info "Dependencies package checking"
+go get -v ${PWD}/commitizen-go
+
+h2Info "Building package"
+go build ${PWD}/commitizen-go ${PWD}/commitizen-go/
+make -C ${PWD}/commitizen-go
+
+
+${PWD}/commitizen-go.exe install
+
+h2Info "Cleaning up..."
+rm -rf commitizen-go
