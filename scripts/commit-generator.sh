82ea3686bdfbcc468cd24452e062ff0e16d03182
rendytantiono <rendy.tantiono@tokopedia.com>
2020-10-01 11:39:55 +0800

    fix: install binary

--- scripts/commit-generator.sh
+++ scripts/commit-generator.sh
@@ -2,18 +2,18 @@
 
 . $(dirname "$0")/utils/chalk.sh
 
-h2Info "Downloading commit generator"
+h2Info "Downloading commit generator..."
 git clone "https://github.com/lintingzhen/commitizen-go"
 
-h2Info "Dependencies package checking"
+h2Info "Dependencies package checking..."
 go get -v ${PWD}/commitizen-go
 
 h2Info "Building package"
-go build ${PWD}/commitizen-go ${PWD}/commitizen-go/
+go build ${PWD}/commitizen-go
 make -C ${PWD}/commitizen-go
 
 
-${PWD}/commitizen-go.exe install
+${PWD}/commitizen-go install
 
 h2Info "Cleaning up..."
 rm -rf commitizen-go
